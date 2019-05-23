6a7c84182ee6745058f01c860120dd8bcd178292
César Richard <cesar.richard@weezevent.com>
2019-05-23 18:55:58 +0200

    Added waterfall async in auth, added logout, prettified

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import { BrowserRouter as Router, Route, Switch, withRouter } from 'react-router-dom';
-import Login from './components/views/public/Login';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { clearAuthState } from "./actions/authActions";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  withRouter
+} from "react-router-dom";
+import Login from "./components/views/public/Login";
+import "./App.css";
 
 class App extends Component {
-
   renderLogin(props) {
     return (
-      <div className = "App" >
-        <header className = "App-header" >
-          <Login {...props}/>
+      <div className="App">
+        <header className="App-header">
+          <Login {...props} />
         </header>
         <span>&copy;2019 - C.Richard</span>
         <br />
@@ -20,14 +25,14 @@ class App extends Component {
     );
   }
   renderMain() {
-    const {auth} = this.props;
+    const { auth } = this.props;
     const isLoggedIn = auth && auth.authState;
 
     if (!isLoggedIn) {
       return (
         <Router>
           <Switch>
-            <Route path="/" render={(props) => this.renderLogin(props)} />
+            <Route path="/" render={props => this.renderLogin(props)} />
           </Switch>
         </Router>
       );
@@ -35,35 +40,47 @@ class App extends Component {
       return this.renderLabel();
     }
   }
-  
 
-renderLabel(){
-  return (
-    <div className="App">
-    <header className="App-header">
-      I used to be a student like you, then I took an arrow in the knee...
-    </header>
-    <span>&copy;2019 - C.Richard</span>
-    <br />
-    <span>Version 0.1</span>
-    <br />
-  </div>)
-}
+  renderLabel() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          I used to be a student like you, then I took an arrow in the knee...
+          <a
+            title="Logout"
+            href="/logout"
+            onClick={event => {
+              event.preventDefault();
+              this.props.clearAuthState();
+            }}
+          >
+            Logout
+          </a>
+        </header>
+        <span>&copy;2019 - C.Richard</span>
+        <br />
+        <span>Version 0.1</span>
+        <br />
+      </div>
+    );
+  }
 
   render() {
-    return (
-      <React.Fragment>
-        {this.renderMain()}
-      </React.Fragment>
-    );
+    return <React.Fragment>{this.renderMain()}</React.Fragment>;
   }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   auth: state.auth
-})
+});
 
-const mapDispatchToProps = (dispatch) => ({
-})
+const mapDispatchToProps = dispatch => ({
+  clearAuthState: () => dispatch(clearAuthState())
+});
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(App)
+);
--- src/components/views/public/Login.js
+++ src/components/views/public/Login.js
@@ -1,13 +1,13 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import { generateAuthState } from '../../../actions/authActions';
-import { createSession } from '../../../actions/sessionActions';
-import {setLoading} from '../../../actions/connectActions';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import queryString from 'query-string';
-import axios from 'axios';
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { generateAuthState } from "../../../actions/authActions";
+import { createSession } from "../../../actions/sessionActions";
+import { setLoading } from "../../../actions/connectActions";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import queryString from "query-string";
+import axios from "axios";
+import { waterfall } from "async";
 
 class Login extends Component {
   constructor(props) {
@@ -31,115 +31,134 @@ class Login extends Component {
   async handleSubmit(event) {
     event.preventDefault();
     this.props.setLoading(true);
+    const SYSTEM_ID = ***REMOVED***;
+    let formData = {
+      service: "http://heimdal",
+      username: this.state.login,
+      password: this.state.password
+    };
 
-    try {
-      const SYSTEM_ID = ***REMOVED***
-      let formData = {
-        service: "http://heimdal",
-        username: this.state.login,
-        password: this.state.password,
-      }
-
-      axios({
-          url: "https://api.nemopay.net/services/ROSETTINGS/getCasUrl",
-          method: "POST",
-          params: {
-            system_id: SYSTEM_ID
-          },
-          headers: {
-            'Content-Type': 'application/json',
-            'Nemopay-Version': '2017-12-15',
-          }
-        })
-        .then(casUrl => {
+    waterfall(
+      [
+        callback => {
           axios({
-              url: "https://cors-anywhere.herokuapp.com/" + casUrl.data + "/v1/tickets/",
-              method: "POST",
-              headers: {
-                'Content-type': 'application/x-www-form-urlencoded',
-                'Accept': 'text/plain'
-              },
-              data: queryString.stringify(formData)
-            })
-            .then(tgt => {
-              console.log(tgt.data);
-              axios({
-                url: "https://cors-anywhere.herokuapp.com/" + casUrl.data + '/v1/tickets/' + tgt.data,
-                method: "POST",
-                headers: {
-                  'Content-type': 'application/x-www-form-urlencoded',
-                  'Accept': 'text/plain'
-                },
-                data: queryString.stringify(formData)
-              })
-              .then(st => {
-                console.log(st.data);
-                axios({
-                  url: "https://api.nemopay.net/services/MYACCOUNT/loginCas2",
-                  method: "POST",
-                  params: {
-                    system_id: SYSTEM_ID
-                  },
-                  headers: {
-                  		'Content-Type': 'application/json',
-                  		'Nemopay-Version': '2017-12-15',
-                  },
-                  data: {
-                    ticket: st.data,
-                    service: formData.service
-                  }
-                })
-                .then(token => {
-                  this.props.generateAuthState();
-                  this.props.createSession({
-                    access_token: token.data
-                  });
-                  this.props.setLoading(false);
-                });
-              })
-              .catch(console.error);
-            })
-            .catch(console.error);
-        })
-        .catch(console.error);
-
-
-    } catch (error) {
-      let wrongPasswordMessage = 'wrongPasswordMessage';
-      let errorTryagain = 'errorTryagain';
-      let message = error.status && error.status === 403 ? wrongPasswordMessage : (error.message || errorTryagain);
-      console.error(message);
-      this.props.setLoading(false);
-    }
+            url: "https://api.nemopay.net/services/ROSETTINGS/getCasUrl",
+            method: "POST",
+            params: {
+              system_id: SYSTEM_ID
+            },
+            headers: {
+              "Content-Type": "application/json",
+              "Nemopay-Version": "2017-12-15"
+            }
+          }).then(data => callback(null, data));
+        },
+        (casUrl, callback) => {
+          axios({
+            url:
+              "https://cors-anywhere.herokuapp.com/" +
+              casUrl.data +
+              "/v1/tickets/",
+            method: "POST",
+            headers: {
+              "Content-type": "application/x-www-form-urlencoded",
+              Accept: "text/plain"
+            },
+            data: queryString.stringify(formData)
+          }).then(data => callback(null, casUrl, data));
+        },
+        (casUrl, tgt, callback) => {
+          axios({
+            url:
+              "https://cors-anywhere.herokuapp.com/" +
+              casUrl.data +
+              "/v1/tickets/" +
+              tgt.data,
+            method: "POST",
+            headers: {
+              "Content-type": "application/x-www-form-urlencoded",
+              Accept: "text/plain"
+            },
+            data: queryString.stringify(formData)
+          }).then(data => callback(null, data));
+        },
+        (st, callback) => {
+          axios({
+            url: "https://api.nemopay.net/services/MYACCOUNT/loginCas2",
+            method: "POST",
+            params: {
+              system_id: SYSTEM_ID
+            },
+            headers: {
+              "Content-Type": "application/json",
+              "Nemopay-Version": "2017-12-15"
+            },
+            data: {
+              ticket: st.data,
+              service: formData.service
+            }
+          }).then(data => callback(null, data));
+        },
+        (token, callback) => {
+          this.props.setLoading(false);
+          this.props.generateAuthState();
+          this.props.createSession({
+            access_token: token.data
+          });
+        }
+      ],
+      (err, res) => {
+        if (err) console.error(err);
+        if (res) console.log(res);
+        this.props.setLoading(false);
+      }
+    );
   }
 
   render() {
-      let loginBody = (
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Group controlId="formLogin">
-            <Form.Control name="login" type="login" placeholder="Login" onChange={this.handleChange} required />
-          </Form.Group>
+    let loginBody = (
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Group controlId="formLogin">
+          <Form.Control
+            name="login"
+            type="login"
+            placeholder="Login"
+            onChange={this.handleChange}
+            required
+          />
+        </Form.Group>
 
-          <Form.Group controlId="formPassword">
-            <Form.Control name="password" type="password" placeholder="Password" onChange={this.handleChange} required />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            {this.props.isLoading() ? "Veuillez patienter..." : "Connexion"}
-          </Button>
-        </Form>
-      );
+        <Form.Group controlId="formPassword">
+          <Form.Control
+            name="password"
+            type="password"
+            placeholder="Password"
+            onChange={this.handleChange}
+            required
+          />
+        </Form.Group>
+        <Button
+          variant="primary"
+          type="submit"
+          disabled={this.props.isLoading() ? "disabled" : ""}
+        >
+          {this.props.isLoading() ? "Veuillez patienter..." : "Connexion"}
+        </Button>
+      </Form>
+    );
 
     return (
       <React.Fragment>
         <div className="text-center">
           <div className={`block-center mt-xl wd-xl login-body `}>
             <div className="panel panel-dark panel-flat">
-              <div className="panel-heading text-center" id="login-page-container">
+              <div
+                className="panel-heading text-center"
+                id="login-page-container"
+              >
                 <h1>Heimdal</h1>
               </div>
-              <div className="panel-body">
-                {loginBody}
-              </div>
+              <div className="panel-body">{loginBody}</div>
               <br />
             </div>
           </div>
@@ -149,16 +168,18 @@ class Login extends Component {
   }
 }
 
-
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   isLoading: () => state.connect.loading,
   auth: state.auth
-})
+});
 
-const mapDispatchToProps = (dispatch) => ({
-  setLoading: (loading) => dispatch(setLoading(loading)),
-  createSession: (auth) => dispatch(createSession(auth)),
+const mapDispatchToProps = dispatch => ({
+  setLoading: loading => dispatch(setLoading(loading)),
+  createSession: auth => dispatch(createSession(auth)),
   generateAuthState: () => dispatch(generateAuthState())
-})
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Login);
