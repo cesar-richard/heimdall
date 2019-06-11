4b4158b81984398c1e341a06e18694d0ef322a98
César Richard <cesar.richard@weezevent.com>
2019-06-11 11:44:16 +0200

    Dotenv

--- src/api/gill/apiServices.js
+++ src/api/gill/apiServices.js
@@ -22,8 +22,8 @@ const request = (endPoint, method, params, headers = {}, forcedParams = {}) => {
   config.params = {
     ...config.params,
     ...forcedParams,
-    system_id: SYSTEMID,
-    app_key: "APIKEY"
+    system_id: process.env.SYSTEM_ID,
+    app_key: process.env.GILL_API_KEY
   };
 
   if (token) {
