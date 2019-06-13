914d20ba61ce5986b85d566778526482a1c732e3
César Richard <cesar.richard@weezevent.com>
2019-06-13 18:27:51 +0200

    Fixed prop-type imports

--- src/components/views/Blocked/Fundation.js
+++ src/components/views/Blocked/Fundation.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
 import { connect } from "react-redux";
 import { getAllBlocked } from "../../../actions/fetch/blockedActions";
 import { Col, Container, Row, ListGroup, Spinner } from "react-bootstrap";
--- src/components/views/Blocked/FundationList.js
+++ src/components/views/Blocked/FundationList.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getFundations } from "../../../actions/fetch";
 import { Spinner, ListGroup } from "react-bootstrap";
