20e1635228bcab09c29ceb72dfbcbb51e24462ba
César Richard <cesar.richard@weezevent.com>
2019-07-22 15:47:14 +0200

    Debug yarn test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from "react-redux";
+import { store } from "./store";
+import { BrowserRouter as Router } from "react-router-dom";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
