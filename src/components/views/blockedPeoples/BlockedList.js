2fd1c520479210c785ab0eb1f0c24a410f8b2169
César Richard <cesar.richard@weezevent.com>
2019-08-17 19:40:03 +0200

    Removed unused semicolon

--- src/components/views/blockedPeoples/BlockedList.js
+++ src/components/views/blockedPeoples/BlockedList.js
@@ -62,7 +62,7 @@ export default function BlockedList(props) {
       {
         <ListGroup>
           {blocked.map((item, key) => {
-            return <BlockedItem key={key} fundationId={fundationId} blockedPeople={item} blockedPeoples={blocked} />;
+            return <BlockedItem key={key} fundationId={fundationId} blockedPeople={item} blockedPeoples={blocked} />
           })}
         </ListGroup>
       }
