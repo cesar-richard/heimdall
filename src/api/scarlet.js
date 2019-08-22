5edf624dfdffd907bd07a9aa5979fd100bcf72f7
César Richard <cesar.richard@weezevent.com>
2019-08-22 16:33:05 +0200

    Scarlet listener lint

--- src/api/scarlet.js
+++ src/api/scarlet.js
@@ -1,7 +1,11 @@
-import openSocket from 'socket.io-client';
-const  socket = openSocket('http://localhost:3001');
-export function subscribeNfc({onDetected=()=>{},onOff=()=>{},onError=()=>{}}) {
-  socket.on('detected', card => onDetected(null, card));
-  socket.on('off', onOff => onOff(null, card));
-  socket.on('error', err => onError(null, err));
+import openSocket from "socket.io-client";
+const socket = openSocket("http://localhost:3001");
+export function subscribeNfc({
+  onDetected = () => {},
+  onOff = () => {},
+  onError = () => {}
+}) {
+  socket.on("detected", card => onDetected(null, card));
+  socket.on("off", onOff => onOff(null, card));
+  socket.on("error", err => onError(null, err));
 }
