fd9648cf0bd724128beecac66f6f8414e822bebf
César Richard <cesar.richard@weezevent.com>
2019-08-27 17:40:53 +0200

    isLoggedIn handling

--- src/components/views/navbar.js
+++ src/components/views/navbar.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Nav, Navbar } from 'react-bootstrap';
+import React from "react";
+import PropTypes from "prop-types";
+import { Nav, Navbar } from "react-bootstrap";
 
-export default function MyNavbar() {
+export default function MyNavbar(props) {
+  const { isLoggedIn } = props;
   return (
     <Navbar bg='primary' variant='dark'>
       <Navbar.Brand href='/'>Heimdal</Navbar.Brand>
-      <Nav className='mr-auto'>
-        <Nav.Link href='/fundations'>Fundations</Nav.Link>
-        <Nav.Link href='/transferts'>Transferts</Nav.Link>
-      </Nav>
+      {isLoggedIn ? (
+        <Nav className='mr-auto'>
+          <Nav.Link href='/fundations'>Fundations</Nav.Link>
+          <Nav.Link href='/transferts'>Transferts</Nav.Link>
+          <Nav.Link href='/logout'>Logout</Nav.Link>
+        </Nav>
+      ) : (
+        <></>
+      )}
     </Navbar>
   );
-};
+}
