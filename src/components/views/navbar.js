c6c01aace6ee80b794a4c957b6204bced2fc97bb
César Richard <cesar.richard@weezevent.com>
2019-08-28 14:20:33 +0200

    pulled infos from package.json

--- src/components/views/navbar.js
+++ src/components/views/navbar.js
@@ -1,18 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Nav, Navbar } from "react-bootstrap";
+import packagejson from "../../../package.json";
 
 export default function MyNavbar(props) {
-  const { isLoggedIn } = props;
+  const { isLoggedIn, username } = props;
   return (
-    <Navbar bg='primary' variant='dark'>
-      <Navbar.Brand href='/'>Heimdal</Navbar.Brand>
+    <Navbar collapseOnSelect expand='sm' bg='primary' variant='dark'>
+      <Navbar.Brand href='/'>{packagejson.name}</Navbar.Brand>
       {isLoggedIn ? (
-        <Nav className='mr-auto'>
-          <Nav.Link href='/fundations'>Fundations</Nav.Link>
-          <Nav.Link href='/transferts'>Transferts</Nav.Link>
-          <Nav.Link href='/logout'>Logout</Nav.Link>
-        </Nav>
+        <>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse
+            id='responsive-navbar-nav'
+            className='justify-content-between'
+          >
+            <Nav className='mr-auto'>
+              <Nav.Link href='/fundations'>Fundations</Nav.Link>
+              <Nav.Link href='/transferts'>Transferts</Nav.Link>
+              <Nav.Link href='/logout'>Logout</Nav.Link>
+            </Nav>
+            <Navbar.Text>
+              Signed in as: <a href='#login'>{username}</a>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </>
       ) : (
         <></>
       )}
