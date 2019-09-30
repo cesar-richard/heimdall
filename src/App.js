169cc11193abaa76efeb0bbb441d94bb0dba795e
César Richard <cesar.richard@weezevent.com>
2019-10-01 00:58:42 +0200

    Fixed config

--- src/App.js
+++ src/App.js
@@ -34,6 +34,14 @@ import {
   faTrafficLight
 } from "@fortawesome/free-solid-svg-icons";
 
+
+window.heimdalConfig = {
+  GILL_BASE_API_URL: process.env.GILL_BASE_API_URL,
+  SYSTEM_ID: process.env.SYSTEM_ID,
+  NEMOPAY_VERSION: process.env.NEMOPAY_VERSION,
+  GILL_APP_KEY: process.env.GILL_APP_KEY
+};
+
 library.add(faBuilding, faSuitcase, faKey, faHandHoldingUsd, faUser, faTrafficLight);
 
 class App extends Component {
