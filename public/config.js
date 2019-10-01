eb30e560e1e834063459127e875d563a3519acbd
César Richard <cesar.richard@weezevent.com>
2019-10-01 10:17:34 +0200

    TO BE DELETED

---
+++ public/config.js
@@ -0,0 +1,6 @@
+window.heimdalConfig = {
+  "GILL_BASE_API_URL": "https://api.nemopay.net",
+  "SYSTEM_ID": "***REMOVED***",
+  "NEMOPAY_VERSION": "***REMOVED***",
+  "GILL_APP_KEY": "***REMOVED***",
+};
