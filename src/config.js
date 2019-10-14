2582d74ce4d9fc9d08bc76559f0635db37d48c99
César Richard <cesar.richard@weezevent.com>
2019-10-14 16:29:28 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ const heimdalConfig = {
   SYSTEM_ID: process.env.SYSTEM_ID || "YOUR_SYSTEM_ID",
   NEMOPAY_VERSION: process.env.NEMOPAY_VERSION || "2019-06-26",
   GILL_APP_KEY: process.env.GILL_APP_KEY || "YOUR_GILL_API_KEY",
-  EVENT_ID: process.env.EVENT_ID || "YOUR_DEFAULT_EVENT_NUMBER"
+  EVENT_ID: process.env.EVENT_ID || "YOUR_DEFAULT_EVENT_NUMBER",
+  test:"test"
 };
 export default heimdalConfig;
