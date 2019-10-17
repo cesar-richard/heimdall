ca7874ff5664e7108da3a6f8c31bdcb29067e301
Cesar Richard <cesar.richard@weezevent.com>
2019-10-17 14:53:54 +0200

    Added base code for finding wallets

--- src/components/views/support/support.js
+++ src/components/views/support/support.js
@@ -1,8 +1,47 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {
+  Alert,
+  Button,
+  Card,
+  Container,
+  ListGroup,
+  Row,
+  Col,
+  Spinner
+} from "react-bootstrap";
+import { initializeSocket, subscribeNfc } from "../../../api/scarlet";
+import { toast } from "react-toastify";
+import Wallet from "../wallet";
+import WalletAutocomplete from "../../WalletAutocomplete";
 
 export default function Support(props) {
-  return <></>;
+  const [readerState, setReaderState] = React.useState("warning");
+  const [sourceCard, setSourceCard] = React.useState(null);
+
+  React.useEffect(() => {
+    initializeSocket();
+    subscribeNfc({
+      onCard: card => {
+        setReaderState("success");
+        setSourceCard(card);
+      },
+      onError: error => {
+        toast.error(error);
+        setReaderState("danger");
+      },
+      onStart: () => setReaderState("success"),
+      onEnd: () => setReaderState("dark")
+    });
+  }, []);
+  return (
+    <Container>
+      <Row>
+        <Alert variant={readerState}>Lecteur NFC</Alert>
+        <WalletAutocomplete />
+      </Row>
+    </Container>
+  );
 }
 
 Support.propTypes = {};
