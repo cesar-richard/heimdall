c6ef82a9d94b28a242bda0987334f3aeae98aede
Cesar Richard <cesar.richard@weezevent.com>
2019-10-29 14:25:58 +0100

    Forbiden page style

--- src/components/views/public/Forbiden.js
+++ src/components/views/public/Forbiden.js
@@ -5,7 +5,14 @@ import { Container, Row } from "react-bootstrap";
 class Forbiden extends Component {
   render() {
     return (
-      <Container>
+      <Container
+        fluid
+        style={{
+          backgroundColor: "#2C3E50",
+          display: "flex",
+          justifyContent: "center"
+        }}
+      >
         <Row>
           <img src='/img/forbiden.png' />
         </Row>
