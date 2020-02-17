70d27055c4149d4dc8fa0a8206e3c7ca78f39951
Cesar Richard <cesar.richard@weezevent.com>
2020-02-17 21:08:11 +0100

    Added Forbiden test

---
+++ src/components/views/public/Forbiden.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Forbiden from "./Forbiden";
+import Enzyme, { mount, render, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new Adapter() });
+describe("Dashboard", () => {
+  it("Render without crashing", () => {
+    const wrapper = shallow(<Forbiden />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
