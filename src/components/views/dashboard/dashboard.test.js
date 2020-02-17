bc0c1e8b05c5ed4cc8f76541755c606e3e4f866a
Cesar Richard <cesar.richard@weezevent.com>
2020-02-17 20:12:42 +0100

    Added tests

---
+++ src/components/views/dashboard/dashboard.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Dashboard from "./dashboard";
+import Enzyme, { mount, render, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new Adapter() });
+describe("Dashboard", () => {
+  it("Render without crashing", () => {
+    const wrapper = shallow(<Dashboard />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
