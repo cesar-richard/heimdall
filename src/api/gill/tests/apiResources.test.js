290651d2cdc1db355b40674dc8b6a4fa687e8333
Cesar Richard <cesar.richard@weezevent.com>
2020-02-19 11:13:36 +0100

    test struct update

--- src/api/gill/tests/apiResources.test.js
+++ src/api/gill/tests/apiResources.test.js
@@ -1,50 +1,145 @@
 import axios from "axios";
 import { DELETE, GET, PATCH, POST, PUT } from "../apiResources";
+import heimdalConfig from "../../../config";
 jest.mock("axios");
 beforeEach(() => {
   localStorage.clear();
+  jest.resetAllMocks();
+  jest.mock("axios");
 });
 
-function test(method, methodName, data) {
+function test(method, methodName, data, expectedCallParams) {
   describe(methodName, () => {
     it("Send requests to API", async () => {
-      const data = { response: { status: 401 } };
       axios.mockImplementationOnce(() => Promise.resolve(data));
       await expect(method("endPoint", [], [])).resolves.toEqual(data);
+      expect(axios).toHaveBeenCalledWith(expectedCallParams);
     });
     it("Send requests to API with forcedParams", async () => {
-      const data = { response: { status: 401 } };
       axios.mockImplementationOnce(() => Promise.resolve(data));
       await expect(method("endPoint", [], [], [])).resolves.toEqual(data);
+      expect(axios).toHaveBeenCalledWith(expectedCallParams);
     });
     it("Send requests to API with sessionid", async () => {
       localStorage.setItem(
         "accessToken",
         JSON.stringify({ sessionid: "abc123" })
       );
-      const data = { response: { status: 401 } };
       axios.mockImplementationOnce(() => Promise.resolve(data));
       await expect(method("endPoint", [], [], [])).resolves.toEqual(data);
+      expect(axios).toHaveBeenCalledWith(expectedCallParams);
     });
     it("Throw Exception on error", async () => {
-      const data = {
-        response: { config: undefined, message: "Gill said : undefined" },
-        status: 401
-      };
       axios.mockImplementationOnce(() => Promise.reject(data));
       await expect(method("endPoint", [], [], [])).rejects.toEqual(data);
+      expect(axios).toHaveBeenCalledWith(expectedCallParams);
     });
     it("Throw Exception on empty error", async () => {
       axios.mockImplementationOnce(() => Promise.reject());
       await expect(method("endPoint", [], [], [])).rejects.toEqual();
+      expect(axios).toHaveBeenCalledWith(expectedCallParams);
     });
   });
 }
 
-describe("apiServices", () => {
-  test(DELETE, "DELETE");
-  test(GET, "GET");
-  test(PATCH, "PATCH");
-  test(POST, "POST");
-  test(PUT, "PUT");
+describe("apiResources", () => {
+  test(
+    DELETE,
+    "DELETE",
+    {
+      response: { config: undefined, message: "Gill said : undefined" },
+      status: 401
+    },
+    {
+      data: [],
+      headers: {
+        "Content-Type": "application/json",
+        "Nemopay-Version": heimdalConfig.NEMOPAY_VERSION
+      },
+      method: "delete",
+      params: {
+        app_key: heimdalConfig.GILL_APP_KEY
+      },
+      url: "/resources/endPoint"
+    }
+  );
+  test(
+    GET,
+    "GET",
+    {
+      response: { config: undefined, message: "Gill said : undefined" },
+      status: 401
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "Nemopay-Version": heimdalConfig.NEMOPAY_VERSION
+      },
+      method: "get",
+      params: {
+        app_key: heimdalConfig.GILL_APP_KEY
+      },
+      url: "/resources/endPoint"
+    }
+  );
+  test(
+    PATCH,
+    "PATCH",
+    {
+      response: { config: undefined, message: "Gill said : undefined" },
+      status: 401
+    },
+    {
+      data: [],
+      headers: {
+        "Content-Type": "application/json",
+        "Nemopay-Version": heimdalConfig.NEMOPAY_VERSION
+      },
+      method: "patch",
+      params: {
+        app_key: heimdalConfig.GILL_APP_KEY
+      },
+      url: "/resources/endPoint"
+    }
+  );
+  test(
+    POST,
+    "POST",
+    {
+      response: { config: undefined, message: "Gill said : undefined" },
+      status: 401
+    },
+    {
+      data: [],
+      headers: {
+        "Content-Type": "application/json",
+        "Nemopay-Version": heimdalConfig.NEMOPAY_VERSION
+      },
+      method: "post",
+      params: {
+        app_key: heimdalConfig.GILL_APP_KEY
+      },
+      url: "/resources/endPoint"
+    }
+  );
+  test(
+    PUT,
+    "PUT",
+    {
+      response: { config: undefined, message: "Gill said : undefined" },
+      status: 401
+    },
+    {
+      data: [],
+      headers: {
+        "Content-Type": "application/json",
+        "Nemopay-Version": heimdalConfig.NEMOPAY_VERSION
+      },
+      method: "put",
+      params: {
+        app_key: heimdalConfig.GILL_APP_KEY
+      },
+      url: "/resources/endPoint"
+    }
+  );
 });
