ee4e8602b8b0ac1df58de65c4f4da2cc1178e12c
Cesar Richard <cesar.richard@weezevent.com>
2020-02-22 17:58:51 +0100

    Tests OFFLINE api

--- src/api/gill/OFFLINE.js
+++ src/api/gill/OFFLINE.js
@@ -1,11 +1,11 @@
 import { POST } from "./apiClient";
 
-export const getConfig = ({ system_id }) => {
+export const getConfig = ({ system_id } = {}) => {
   if (system_id === undefined) throw new Error("missing parameters");
   return POST("services", "OFFLINE/getConfig", {}, {}, { system_id });
 };
 
-export const getCounterSchemas = ({ system_id, event_id }) => {
+export const getCounterSchemas = ({ system_id, event_id } = {}) => {
   if (system_id === undefined || event_id === undefined)
     throw new Error("missing parameters");
   return POST(
@@ -17,7 +17,7 @@ export const getCounterSchemas = ({ system_id, event_id }) => {
   );
 };
 
-export const getWalletConfigs = ({ system_id, event_id }) => {
+export const getWalletConfigs = ({ system_id, event_id } = {}) => {
   if (system_id === undefined || event_id === undefined)
     throw new Error("missing parameters");
   return POST(
@@ -29,7 +29,7 @@ export const getWalletConfigs = ({ system_id, event_id }) => {
   );
 };
 
-export const getWifiConfigs = ({ system_id, event_id }) => {
+export const getWifiConfigs = ({ system_id, event_id } = {}) => {
   if (system_id === undefined || event_id === undefined)
     throw new Error("missing parameters");
   return POST(
--- src/api/gill/tests/resources.test.js
+++ src/api/gill/tests/resources.test.js
@@ -17,31 +17,6 @@ beforeEach(() => {
   jest.mock("../apiClient");
 });
 
-const errorReal = {
-  config: {
-    baseURL: "https://api.nemopay.net",
-    data: undefined,
-    headers: {
-      Accept: "application/json, text/plain, */*",
-      "Nemopay-Version": "2019-06-26"
-    },
-    maxContentLength: -1,
-    method: "get",
-    params: {
-      app_key: "0a93e8e18e6ed78fa50c4d74e949801b",
-      event_id: 1,
-      ordering: "name",
-      system_id: 160677
-    },
-    timeout: 0,
-    url: "/resources/fundations",
-    xsrfCookieName: "XSRF-TOKEN",
-    xsrfHeaderName: "X-XSRF-TOKEN"
-  },
-  message: "Gill said : undefined",
-  status: 401
-};
-
 function testMethod(
   apiMethod,
   method,
