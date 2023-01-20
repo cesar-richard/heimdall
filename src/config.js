be3b7b94ec2ffb5b98b1207d0f7119f83b12ab9d
César Richard <cesar@crichard.fr>
2023-01-20 12:49:15 +0100

    Cleanup

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const heimdalConfig = {
   GILL_BASE_API_URL: process.env.GILL_BASE_API_URL || "https://api.nemopay.net",
   NEMOPAY_VERSION: process.env.NEMOPAY_VERSION || "2019-06-26",
-  GILL_APP_KEY: process.env.GILL_APP_KEY || "YOUR_GILL_API_KEY",
+  GILL_APP_KEY: process.env.GILL_APP_KEY || "0a93e8e18e6ed78fa50c4d74e949801b",
 };
 export default heimdalConfig;
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import App from "../App";
-import Enzyme, { mount, render, shallow } from "enzyme";
+import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new Adapter() });
